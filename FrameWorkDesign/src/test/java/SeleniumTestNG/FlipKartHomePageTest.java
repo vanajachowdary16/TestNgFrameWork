0e66439cb982cc937d55aa6780dadd36ac3ef8d6
vanajachowdary16 <vnajachowdary12@gmail.com>
2025-06-03 15:24:08 +0530

    added files

--- FrameWorkDesign/src/test/java/SeleniumTestNG/FlipKartHomePageTest.java
+++ FrameWorkDesign/src/test/java/SeleniumTestNG/FlipKartHomePageTest.java
@@ -6,16 +6,20 @@ import java.time.Duration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import AbstractComponents.AbstractComponent;
+import PageObjects.HomeNavigationPageObjects;
 import PageObjects.LandingPage;
+import Test.Uitilities.WebUtilis;
+import TestUtility.BaseTest;
 import TestUtility.TestWebDriverUtility;
 
-public class FlipKartHomePageTest extends TestWebDriverUtility{
-	TestWebDriverUtility testWebDriverUtility = new TestWebDriverUtility();
+public class FlipKartHomePageTest extends BaseTest{
 	WebDriverWait wait;	
 	LandingPage landingPage;
 	
---
+++ FrameWorkDesign/src/test/java/SeleniumTestNG/FlipkartHomepageNavigationsTest.java
@@ -0,0 +1,31 @@
+package SeleniumTestNG;
+
+import java.io.IOException;
+
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import PageObjects.HomeNavigationPageObjects;
+import PageObjects.LandingPage;
+import TestUtility.TestWebDriverUtility;
+
+public class FlipkartHomepageNavigationsTest extends TestWebDriverUtility{
+	
+	WebDriverWait wait;	
+	LandingPage landingPage;
+	HomeNavigationPageObjects homenavigations;
+	
+	@BeforeTest
+	public void setUp() throws IOException {
+		landingPage = launchApplication();
+		homenavigations = new HomeNavigationPageObjects(driver);
+		
+	}
+	@Test
+	public void testNavigations() {
+		homenavigations.clickFashionDropdown();
+		
+	}
+
+}
