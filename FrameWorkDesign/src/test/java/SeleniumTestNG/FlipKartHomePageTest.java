b58e4fc49289d89f17b616717b48ea7a76fa7e5b
vanajachowdary16 <vnajachowdary12@gmail.com>
2025-06-14 14:19:32 +0530

    reports

--- FrameWorkDesign/src/test/java/SeleniumTestNG/FlipKartHomePageTest.java
+++ FrameWorkDesign/src/test/java/SeleniumTestNG/FlipKartHomePageTest.java
@@ -1,23 +1,13 @@
 package SeleniumTestNG;
 
 import java.io.IOException;
-import java.time.Duration;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import AbstractComponents.AbstractComponent;
-import PageObjects.HomeNavigationPageObjects;
 import PageObjects.LandingPage;
-import Test.Uitilities.WebUtilis;
 import TestUtility.BaseTest;
-import TestUtility.TestWebDriverUtility;
+
 
 public class FlipKartHomePageTest extends BaseTest{
 	WebDriverWait wait;	
